048b92277113a30bd20c5b8d19d66717e2b185a8
filiptASC6 <filipjamest@gmail.com>
2019-07-12 09:58:31 -0400

    Creates function with return value

--- returnPractice.js
+++ returnPractice.js
@@ -10,3 +10,17 @@ result = someFunc ();
 console.log (result);
 //by default all javascript functions return a value
 // if a value is not specified the function returns undefined
+
+function addTwoNums(num1, num2){
+    return num1 + num2;
+}
+
+const sum = addTwoNums(999, 54056);
+console.log (sum);
+
+function returnValuePrac() {
+    return (1==0)
+    //returns false, because 1 is not equal to 0
+}
+console.log(returnValuePrac());
+
